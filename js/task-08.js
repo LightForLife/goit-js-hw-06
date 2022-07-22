3463db7ccc50611ae84f2487ef1ed3f460914fd1
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2022-07-22 15:16:02 +0300

    add solution task_8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+// ==================== Solution =========================== //
 
+const form = document.querySelector("form.login-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (email === "" || password === "") {
+    return alert("Все поля должны быть заполнены :)");
+  } else {
+    const formData = {
+      email,
+      password,
+    };
+
+    console.log(formData);
+
+    event.currentTarget.reset();
+  }
+}
