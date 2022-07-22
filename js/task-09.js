678e834fb8c56ea2a34f751c64192b2f457f8e18
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2022-07-22 16:07:53 +0300

    add solution task_9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+// ==================== Solution =========================== //
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const bodyEl = document.querySelector("body");
+const buttonChangeColor = document.querySelector("button.change-color");
+const valueColor = document.querySelector("span.color");
+
+buttonChangeColor.addEventListener("click", buttonChangeColorClickListener);
+
+function buttonChangeColorClickListener() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  valueColor.innerHTML = getRandomHexColor();
 }
